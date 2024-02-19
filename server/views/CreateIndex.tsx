f8ef028837b2dfe665fd229c93fc14f797c5c34d
Claudio Cicali <claudio.cicali@gmail.com>
2024-02-19 21:36:04 +0100

    More css

--- server/views/CreateIndex.tsx
+++ server/views/CreateIndex.tsx
@@ -1,12 +1,12 @@
 import { Layout } from './Layout';
 import { PageProps } from '../types';
-// import styles from './CreateIndex.module.css';
+import styles from './CreateIndex.module.css';
 
 export const CreateIndex = ({ title }: PageProps) => (
   <Layout title={`Creating the home page`}>
-    <form action="" method="post">
+    <form action="" method="post" className={styles.test}>
       <button class="button primary" type="submit">
-        Save and close
+        Save and close!!!
       </button>
       <div id="editor-placeholder">
         <h1>{title}</h1>
