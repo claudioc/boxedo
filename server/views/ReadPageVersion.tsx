6b06af4582a5418b06d7855082de735bcabe9693
Claudio Cicali <claudio.cicali@gmail.com>
2024-05-13 18:47:18 +0200

    Hide the history notice

--- server/views/ReadPageVersion.tsx
+++ server/views/ReadPageVersion.tsx
@@ -17,20 +17,29 @@ export const ReadPageVersion = ({
 }: ReadPageVersionProps) => (
   <Layout title={item.pageTitle} page={page}>
     <div>
-      <div class="message is-info" role="alert">
-        <p class="message-header">
-          This is an older version (#{version.split('-')[0]}) of this page
-        </p>
+      <div class="message is-info" role="alert" x-ref="message">
+        <div class="message-header">
+          <p>
+            This is an older version (#{version.split('-')[0]}) of this page
+          </p>
+          <button
+            class="delete"
+            aria-label="delete"
+            x-on:click="$refs.message.hidden = true"
+          ></button>
+        </div>
         <div class="message-body">
-          This version was last saved on{' '}
-          <strong>{formatDate(item.updatedAt)}</strong> while the newest version
-          is from <strong>{formatDate(page.updatedAt)}</strong>. You can view
-          the current version following{' '}
-          <a class="is-link" href={slugUrl(page.pageSlug)}>
-            this link
-          </a>
-          , or you can go back to the{' '}
-          <a href={`../${page._id}`}>list of all its versions</a>.
+          <p>
+            This version was last saved on{' '}
+            <strong>{formatDate(item.updatedAt)}</strong> while the newest
+            version is from <strong>{formatDate(page.updatedAt)}</strong>. You
+            can view the current version following{' '}
+            <a class="is-link" href={slugUrl(page.pageSlug)}>
+              this link
+            </a>
+            , or you can go back to the{' '}
+            <a href={`../${page._id}`}>list of all its versions</a>.
+          </p>
           <details>
             <summary>More …</summary>
             <p>
