5ee8d7e27a3ec3d3a8b91c6ad126d2e39af1afe2
Claudio Cicali <claudio.cicali@gmail.com>
2025-03-16 19:09:02 +0100

    chore: added husky and commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,2 @@
-export default {extends: ['@commitlint/config-conventional']};
+// Docs: https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
+export default { extends: ['@commitlint/config-conventional'] };
