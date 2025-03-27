32587f7d347df0d52303fd540aef2a61e80c9847
Claudio Cicali <claudio.cicali@gmail.com>
2025-03-27 19:26:16 +0100

    chore(core): removes vite.config.ts

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
+import { config } from 'dotenv';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  test: {
-    setupFiles: ['./vitest.setup.ts'],
-  },
 });
+
+config();
